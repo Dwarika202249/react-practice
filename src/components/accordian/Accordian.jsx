5301d2ab49f41d3d3c412819815426af8debc4cf
Dwarika Kumar <dwarikabppimt202249@gmail.com>
2024-01-23 18:35:59 +0530

    accordian

---
+++ src/components/accordian/Accordian.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import "./accordian.css";
+import data from "./accordiandata.js";
+
+const Accordian = () => {
+
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([])
+
+  function handleSingleSelection (getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId)
+  }
+
+  function handleMultiSelection(getCurrentId) {
+    let copyMultiple = [...multiple];
+    const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+    if(findIndexOfCurrentId === -1) {
+      copyMultiple.push(getCurrentId);
+    } else {
+      copyMultiple.splice(findIndexOfCurrentId, 1);
+    }
+
+    setMultiple(copyMultiple);
+  }
+
+  useEffect(() => {
+    setSelected(null)
+    setMultiple([])
+  }, [enableMultiSelection])
+  
+
+  return (
+    <div className="wrapper">
+      <h1 className="project1">PROJECT 1 - ACCORDIAN</h1>
+      <button onClick={()=> setEnableMultiSelection(!enableMultiSelection)} className={enableMultiSelection ? "enableMulti" : "disableMulti"} >{enableMultiSelection ? "Disable Multi Selection" : "Enable Multi Selection"}</button>
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div onClick={enableMultiSelection ? ()=> handleMultiSelection(dataItem.id) : () => handleSingleSelection(dataItem.id)} className="title">
+                <h3>{dataItem.question}</h3>
+                {selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? <div className="sign">-</div> : <div className="sign">+</div> }
+              </div>
+              {
+                enableMultiSelection ? multiple.indexOf(dataItem.id) !== -1 && (<div className="answer">{dataItem.answer}</div>) : selected === dataItem.id && (<div className="answer">{dataItem.answer}</div>)
+                // selected === dataItem.id || multiple.indexOf(dataItem.id) !== -1 ? <div className="answer">{dataItem.answer}</div> : null
+              }
+            </div>
+          ))
+        ) : (
+          <div>Data not found !!</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Accordian;
---
+++ src/components/accordian/accordiandata.js
@@ -0,0 +1,25 @@
+const accordianData = [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      answer: "The capital of France is Paris."
+    },
+    {
+      id: 2,
+      question: "In which year did World War II end?",
+      answer: "World War II ended in the year 1945."
+    },
+    {
+      id: 3,
+      question: "What is the largest planet in our solar system?",
+      answer: "The largest planet in our solar system is Jupiter."
+    },
+    {
+      id: 4,
+      question: "Who wrote the play 'Romeo and Juliet'?",
+      answer: "The play 'Romeo and Juliet' was written by William Shakespeare."
+    },
+    // Add more objects as needed
+  ];
+
+  export default accordianData;
